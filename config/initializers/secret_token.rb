75fcffd19d3a0a19e1e047224a9f8e830f94e955
Morgan Mikel McDaris <morganmcdaris@yahoo.com>
2012-04-13 16:38:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesterAir::Application.config.secret_token = '31ff27f7faa5325270d4fc441f3aa9f9fca64cc5b51d261b29e712694dca9ff4019522c98411fd6df11d272ed97b92ad398e3a383d266c6b460c3056627f720e'
